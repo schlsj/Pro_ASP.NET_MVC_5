3a4a3de9f0d69cf6e88a4d68fa64e57b80521dd5
emiyou <674299035@qq.com>
2018-05-23 23:35:14 +0800

    完成Razor基本语法
    你与真想之间，只差一个百度。
    感谢https://www.cnblogs.com/qixuejia/p/5211175.html

---
+++ Chapter04/Ch04/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct=new Product();
+            myProduct.Namge = "kayak";
+            string productName = myProduct.Namge;
+            return View("Result", (object) string.Format("Product name: {0}", productName));
+        }
+    }
+
+    public class ShpiingCard : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product product in productEnum)
+            {
+                total += product.Price;
+            }
+            return total;
+        }
+    }
+}
---
+++ Chapter04/Ch04/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        private string name;
+
+        public string Namge
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal Price { get; set; }
+    }
+}
